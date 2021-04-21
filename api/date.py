ef906c586bf86d80950cf771f0adaf7e7e584cc1
gcf0082 <13510989573@sina.cn>
2021-04-21 20:18:42 +0800

    Update date.py

--- api/date.py
+++ api/date.py
@@ -7,5 +7,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('gcf  %Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(str(datetime.now().strftime('gcfxxx  %Y-%m-%d %H:%M:%S')).encode())
     return
